3da6a08dd3d89ab8d6ad43b91aa493ffe55dfb18
rubenspotcake <rubenspotcake@proton.me>
2024-06-06 15:59:29 -0300

    Primeiro commit auth-server

---
+++ src/main/java/com/rubens/rubens/RubensApplication.java
@@ -0,0 +1,13 @@
+package com.rubens.rubens;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RubensApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RubensApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rubens/rubens/RubensApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rubens.rubens;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RubensApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
